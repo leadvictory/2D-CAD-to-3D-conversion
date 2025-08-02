fc47b143fb3809db72cef870fb604012daa40603
boris <jkscur@gmail.com>
2025-08-01 19:16:47 -0500

    update app.py to english & cad_code_generator

--- app.py
+++ app.py
@@ -17,22 +17,22 @@ def parse_args():
 
 args = parse_args()
 
-st.title("2D図面 to 3DCAD")
+st.title("2D Drawing to 3D CAD")
 
-uploaded_file = st.sidebar.file_uploader("画像ファイルを選択してください", type=["jpg", "jpeg", "png"])
+uploaded_file = st.sidebar.file_uploader("Select an image file", type=["jpg", "jpeg", "png"])
 
-# 画像がアップロードされたら表示
+# Display the image if uploaded
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     ext = os.path.splitext(uploaded_file.name)[1]
-    st.image(image, caption="アップロードされた画像", use_column_width=True)
-    st.write("画像のサイズ: ", image.size)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.write("Image size: ", image.size)
     with open(f"temp.{ext}", "wb") as f:
         f.write(uploaded_file.getbuffer())
-    with st.spinner("画像を処理中..."):
+    with st.spinner("Processing image..."):
         generate_step_from_2d_cad_image(
             f"temp.{ext}", "output.step", model_type=args.model_type
         )
-    st.success("3DCADデータの生成が完了しました。")
+    st.success("3D CAD data has been successfully generated.")
 else:
-    st.write("画像がアップロードされていません。")
+    st.write("No image uploaded.")
--- cad3dify/v1/cad_code_generator.py
+++ cad3dify/v1/cad_code_generator.py
@@ -275,7 +275,7 @@ class CadCodeGeneratorChain(SequentialChain):
         gen_cad_code_prompt = (
             "You are a highly skilled CAD designer. Please write code that converts the attached 2D CAD image into a 3D CAD model using a Python CAD library called 'cadquery.'\n"
             "## Points to Note\n"
-            "* Please use the `cadquery.exporters.export` function to output the created 3D model as a STEP file.\n"
+            "* Please use the `from cadquery import exporters` statement and then the `exporters.export` function to output the created 3D model as a STEP file.\n"
             "* Where you describe the output file path, use the template string `{{output_filename}}`. The 'output_filename' includes the file extension.\n"
             "* Surround the code with a markdown code block.\n"
             "* Refer to the sample code for how to use Cadquery.\n"
